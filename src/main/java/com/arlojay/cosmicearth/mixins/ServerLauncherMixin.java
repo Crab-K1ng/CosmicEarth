c8ea2bf7774d79df44ec7b178848c3c1935147ab
Crab_King <127689590+Crab-K1ng@users.noreply.github.com>
2024-12-12 02:29:04 +0000

    remove unneeded comments

--- src/main/java/com/arlojay/cosmicearth/mixins/ServerLauncherMixin.java
+++ src/main/java/com/arlojay/cosmicearth/mixins/ServerLauncherMixin.java
@@ -11,18 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLauncher.class)
 public class ServerLauncherMixin {
-//    @ModifyVariable(method = "main", at = @At(value = "STORE", target = "Lfinalforeach/cosmicreach/io/ChunkLoader;loadWorld(Ljava/lang/String;)Lfinalforeach/cosmicreach/world/World;"), ordinal = 0)
-//    private static String injected(String selectedZoneId) {
-//        return "cosmicearth:earth";
-//    }
-
     @ModifyVariable(method = "main", at = @At("STORE"), ordinal = 1)
     private static String injected2(String selectedZoneId) {
         return "cosmicearth:earth";
     }
-
-//    @ModifyVariable(method = "main", at = @At(value = "INVOKE", target = "Lfinalforeach/cosmicreach/io/ChunkLoader;loadWorld(Ljava/lang/String;)Lfinalforeach/cosmicreach/world/World;"), ordinal = 0)
-//    private static String injected(String selectedZoneId) {
-//        return "cosmicearth:earth";
-//    }
 }
